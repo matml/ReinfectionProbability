faf019e919e699d1497191f154c31e5a2ab8dbad
Anton <anton.camacho@lshtm.ac.uk>
2016-07-06 14:31:57 +0100

    wrapper function for every model

--- R/dev.r
+++ R/dev.r
@@ -46,9 +46,67 @@ p_reinfection_PPI <- function(n_reinfection, R0, D_infection, partial_protection
 
 }
 
+logLike_0123 <- function(data = c(n_0 = 11, n_1 = 181, n_2 = 92, n_3_or_more = 0), p_reinfection, ..., echo = FALSE) {
+
+	# prob n_reinfection >=1, >= 2, >=3
+	p_1_or_more <- p_reinfection(n_reinfection = 1, ...)
+	p_2_or_more <- p_reinfection(n_reinfection = 2, ...)
+	p_3_or_more <- p_reinfection(n_reinfection = 3, ...)
+	
+	# prob exactly 0, 1, 2 reinfections
+	p_0 <- 1 - p_1_or_more
+	p_1 <- p_1_or_more - p_2_or_more
+	p_2 <- p_2_or_more - p_3_or_more
+
+	# multinomial likelihood observation
+	logLike <- dmultinom(x = data, prob = c(p_0, p_1, p_2, p_3_or_more), log = TRUE)
+
+	if(echo){
+		E <- round(c(p_0, p_1, p_2, p_3_or_more)*sum(data))
+		cat(paste(c("E[0] =", "E[1] =", "E[2] =", "E[3+] =", "logLike = "), c(E,round(logLike, 3)), collapse = "; "), "\t")
+	}
+
+	return(logLike)
+
+}
+
+logLike_SIRS <- function(x, data, D_infection, n_pop, S_0, I_0, R_0) {
+
+	# x contains R0 and D_immunity
+	R0 <- x[1]
+	D_immunity <- x[2]
+
+	logLike_0123(data, p_reinfection_SIRS, R0, D_infection, D_immunity, n_pop, S_0, I_0, R_0)
+
+}
+
+
+logLike_AoN <- function(x, data, D_infection, n_pop, S_0, I_0, R_0) {
+
+	# x contains R0 and prop_immunity
+	R0 <- x[1]
+	prop_immunity <- x[2]
+
+	logLike_0123(data, p_reinfection_AoN, R0, D_infection, prop_immunity, n_pop, S_0, I_0, R_0)
+
+}
+
+
+logLike_PPI <- function(x, data, D_infection, n_pop, S_0, I_0, R_0) {
+
+	# x contains R0 and partial_protection
+	R0 <- x[1]
+	partial_protection <- x[2]
+
+	logLike_0123(data, p_reinfection_PPI, R0, D_infection, partial_protection, n_pop, S_0, I_0, R_0, echo = TRUE)
+
+}
+
+
+
 test <- function() {
 
-	n_reinfection <- 5
+	n_reinfection <- 2
 	
 	R0 <- 10
 	D_infection <- 3
@@ -57,13 +115,44 @@ test <- function() {
 	I_0 <- 1
 	R_0 <- 0
 
-	D_immunity <- 20
-	prop_immunity <- 0.5
-	partial_protection <- 0.9
+	D_immunity <- 60
+	prop_immunity <- 0.45
+	partial_protection <- 0.95
 
-	p1 <- p_reinfection_SIRS(n_reinfection, R0, D_infection, D_immunity, n_pop, S_0, I_0, R_0)
-	p2 <- p_reinfection_AoN(n_reinfection, R0, D_infection, prop_immunity, n_pop, S_0, I_0, R_0)
-	p3 <- p_reinfection_PPI(n_reinfection, R0, D_infection, partial_protection, n_pop, S_0, I_0, R_0)
+	# p1 <- p_reinfection_SIRS(n_reinfection, R0, D_infection, D_immunity, n_pop, S_0, I_0, R_0)
+	# p2 <- p_reinfection_AoN(n_reinfection, R0, D_infection, prop_immunity, n_pop, S_0, I_0, R_0)
+	# p3 <- p_reinfection_PPI(n_reinfection, R0, D_infection, partial_protection, n_pop, S_0, I_0, R_0)
+
+	data <- c(n_0 = 11, n_1 = 181, n_2 = 92, n_3_or_more = 0)
+	logLike_0123(data, p_reinfection_SIRS, R0, D_infection, D_immunity, n_pop, S_0, I_0, R_0, echo = TRUE)
+	logLike_0123(data, p_reinfection_AoN, R0, D_infection, prop_immunity, n_pop, S_0, I_0, R_0, echo = TRUE)
+	logLike_0123(data, p_reinfection_PPI, R0, D_infection, partial_protection, n_pop, S_0, I_0, R_0, echo = TRUE)
+
+	df_PPI_grid <- expand.grid(R0 = 1:20, partial_protection = seq(0,1,0.1))
+	df_PPI_logLike_grid <- df_PPI_grid %>% group_by(R0, partial_protection) %>% do(logLike = logLike_PPI(x = c(.$R0, .$partial_protection), data = data, D_infection = D_infection, n_pop = n_pop, S_0 = S_0, I_0 = I_0, R_0 = R_0))
+
+
+	df_PPI_grid <- expand.grid(R0 = 1:20, partial_protection = seq(0,1,0.1))
+	df_PPI_logLike_grid <- df_PPI_grid %>% group_by(R0, partial_protection) %>% do(logLike = logLike_PPI(x = c(.$R0, .$partial_protection), data = data, D_infection = D_infection, n_pop = n_pop, S_0 = S_0, I_0 = I_0, R_0 = R_0))
+
+	# df_SIRS_grid <- expand.grid(R0 = 1:20, D_immunity = seq(5, 50, 5))
+	# df_SIRS_logLike_grid <- df_SIRS_grid %>% group_by(R0, D_immunity) %>% do(logLike = logLike_SIRS(x = c(.$R0, .$D_immunity), data = data, D_infection = D_infection, n_pop = n_pop, S_0 = S_0, I_0 = I_0, R_0 = R_0))
+
+	df_AoN_grid <- expand.grid(R0 = 1:20, prop_immunity = seq(0,1,0.1))
+	df_AoN_logLike_grid <- df_AoN_grid %>% group_by(R0, prop_immunity) %>% do(logLike = logLike_AoN(x = c(.$R0, .$prop_immunity), data = data, D_infection = D_infection, n_pop = n_pop, S_0 = S_0, I_0 = I_0, R_0 = R_0))
+
+	library(tidyr)
+	df_AoN_logLike_grid2 <- df_AoN_logLike_grid %>% unnest(logLike) %>% mutate(model = "AoN") %>% rename(y = prop_immunity)
+	df_PPI_logLike_grid2 <- df_PPI_logLike_grid %>% unnest(logLike) %>% mutate(model = "PPI") %>% rename(y = partial_protection)
+
+	df_models_logLike <- df_AoN_logLike_grid2 %>% bind_rows(df_PPI_logLike_grid2)
+	
+	p <- ggplot(df_models_logLike, aes(x = R0, y = y)) + facet_wrap(~model, scales = "free_y")
+	p <- p + geom_raster(aes(fill = log(-logLike)))
+	p <- p + stat_density_2d(aes(fill = logLike), geom = "polygon")
+	p
+	# logLike_PPI(x = c(R0, partial_protection), data = data, D_infection = D_infection, n_pop = n_pop, S_0 = S_0, I_0 = I_0, R_0 = R_0)
+	# optim(par = c(R0, partial_protection), fn = logLike_PPI, data = data, D_infection = D_infection, n_pop = n_pop, S_0 = S_0, I_0 = I_0, R_0 = R_0, method = "L-BFGS-B", lower = c(0,0), upper = c(20, 1))
 
 }
 
