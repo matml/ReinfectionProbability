ec5ccbf963078de7272fa6f78a9419deb512eb46
Anton <anton.camacho@lshtm.ac.uk>
2017-02-24 13:59:25 +0100

    try different R

--- perl/run_condor.pl
+++ perl/run_condor.pl
@@ -24,7 +24,7 @@ my $Rscript= "dev.r";
 # my @node_list= ("01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20");
 my @node_list= ("19");
 
-my $exe= "$HOME/installed/R-3.3.2/bin/R";
+my $exe= "$HOME/bin/R";
 my $uni= "vanilla";
 my $mem= "1000";
 my $replicate= "1";
